2587f3060e1b3926b506eca446d057edb41c66a4
qtt <qintangtao@apkshow.com>
2018-08-15 16:22:25 +0800

    add apktool/apktool_2.3.3.jar and build.bat

---
+++ apktool/build.bat
@@ -0,0 +1,30 @@
+@echo off
+
+echo ------------------------------------
+call apktool_2.3.3.bat b %1 -o %1-new.apk
+
+echo ------------------------------------
+for %%x in (classes*.dex) do (
+	del %%x
+)
+
+echo ------------------------------------
+for %%x in (%1\build\apk\classes*.dex) do (
+	xcopy /y %%x .\
+)
+
+echo ------------------------------------
+for %%x in (classes*.dex) do (
+	aapt remove %1.apk %%x
+	aapt add %1.apk %%x
+)
+
+echo ------------------------------------
+del %1-new.apk
+for %%x in (classes*.dex) do (
+	del %%x
+)
+
+echo ------------------------------------
+copy /y %1.apk ..\kstools\src.apk
+
---
+++ apktool/build_tencent.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call build.bat TencentVideo_V6.2.2.17134_848
