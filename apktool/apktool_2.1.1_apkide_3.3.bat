3b8686c70c5d60ab0cd2be38d85089afc82ad133
qtt <qintangtao@apkshow.com>
2018-08-22 11:24:56 +0800

    del bat

--- apktool/apktool_2.1.1_apkide_3.3.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%CD%;%PATH%;
-java -jar "%~dp0\apktool_2.1.1_apkide_3.3.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
--- apktool/apktool_2.2.2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%CD%;%PATH%;
-java -jar "%~dp0\apktool_2.2.2.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
